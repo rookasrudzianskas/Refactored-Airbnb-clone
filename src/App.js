649a3b173f1ecfa3a5c2d85354043ab2b5ad4802
Rokas <rookas.rudzenskas@gmail.com>
2021-04-27 22:12:58 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="app">
         <Header />
-            {/*<Home />*/}
+            <Home />
     {/*  Home */}
       {/*  Header */}
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './styles/Home.css';
+import Banner from "./Banner";
 
 const Home = () => {
     return (
         <div className="home">
             <h1>Home component</h1>
+            <Banner />
         </div>
     )
 }
