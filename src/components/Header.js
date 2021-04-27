eb15cd133f82ea5e9f68c90973c3c63532adcb2e
Rokas <rookas.rudzenskas@gmail.com>
2021-04-27 21:48:26 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,42 @@
 import React from "react";
 import './styles/Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar } from "@material-ui/core";
 
 const Header = () => {
     return (
+
+        // Header component
+
         <div className="header">
+
+            {/* THe section left lohgo*/}
+
             <img src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" className="header__icon" alt=""/>
+
+            {/*endof logo*/}
+
+            {/* the center section of header    */}
+
+            <div className="header__center">
+                <input type="text"/>
+                <SearchIcon />
+            </div>
+
+            {/* the end of center section of header */}
+
+            {/* the section right   */}
+
+            <div className="header__right">
+                <p>Become a host</p>
+                <LanguageIcon />
+                <ExpandMoreIcon />
+                <Avatar />
+            </div>
+
+        {/*    the end of the section right     */}
         </div>
     )
 }
