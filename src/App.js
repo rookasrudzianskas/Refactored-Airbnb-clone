32583226f474d633795471e8f5a4c1841c244840
Rokas <rookas.rudzenskas@gmail.com>
2021-04-28 08:07:11 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -2,25 +2,34 @@ import './App.css';
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchPage from "./components/SearchPage";
+
 
 function App() {
   return (
     <div className="app">
-        <Header />
-            <Home />
+        <Router>
+            <Header />
 
-        <Footer />
-    {/*  Home */}
-      {/*  Header */}
+            <Switch>
 
-      {/*  Banner */}
-        {/*  Seach */}
+                <Route path="/search">
+                    {/* search page */}
+                    <SearchPage />
+                </Route>
 
-      {/*  footer */}
+                <Route path="/">
+                    {/* / */}
+                    <Home />
+                </Route>
+
+            </Switch>
+
+            <Footer />
+        </Router>
 
 
-    {/*  Search Page*/}
-      {/*  .............  */}
     </div>
   );
 }
---
+++ src/components/SearchPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles/SearchPage.css";
+
+
+const SearchPage = () => {
+    return (
+        <div className="searchPage">
+            <h1>I am the search page 🙈</h1>
+        </div>
+    )
+}
+
+export default SearchPage;
