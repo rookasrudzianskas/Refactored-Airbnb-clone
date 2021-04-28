5b0e7441ed933a52afaf0068eb86525f296f881f
Rokas <rookas.rudzenskas@gmail.com>
2021-04-28 08:17:29 +0300

    Rokas

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./styles/SearchPage.css";
+import {Button} from "@material-ui/core";
 
 
 const SearchPage = () => {
     return (
         <div className="searchPage">
-            <h1>I am the search page 🙈</h1>
+            <div className="searchPage__info">
+                <p>62 stays · 26 august to 30 august · 2 guest</p>
+                <h1>Stays nearby</h1>
+                <Button variant="outlined">Cancellation Flexibility</Button>
+                <Button variant="outlined">Type of place</Button>
+                <Button variant="outlined">Price</Button>
+                <Button variant="outlined">Rooms and beds</Button>
+                <Button variant="outlined">More filters</Button>
+            </div>
         </div>
     )
 }
